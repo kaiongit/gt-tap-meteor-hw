1683a30cf27c02fcd5443ba8bd8ea0bc12175d44
kaiongit <60166885+kaiongit@users.noreply.github.com>
2022-07-11 20:58:20 +0800

    Update routing to only allow a fixed combination of method and path

--- main.py
+++ main.py
@@ -9,27 +9,38 @@ import functions_framework  # Only for local development, do not include in depl
 def gov_grant_disb(request):
     
     allowed_methods: tuple = ("POST", "GET")
-    allowed_paths: tuple = ("/create_hh", "/add_mb", "/search_hh", "/list_grants")
+
+    allowed_requests: list = [
+        {"method": "POST", "path": "/create_hh"},
+        {"method": "POST", "path": "/add_mb"},
+        {"method": "GET", "path": "/search_hh"},
+        {"method": "GET", "path": "/list_grants"},
+    ]
+
+    method = request.method
 
     # Check if method is allowed
-    if request.method not in allowed_methods:
+    if method not in allowed_methods:
         return {"msg": "Unknown request"}, 405
 
+    path = request.path
+    allowed = False
+
+    for allowed_request in allowed_requests:
+        if method == allowed_request["method"] and path == allowed_request["path"]:
+            allowed = True
+
     # Check if path is allowed/implemented
-    if request.path not in allowed_paths:
+    if not allowed:
         return {"msg": "Unknown request"}, 400
 
-    # Route to each path's function
-    function = request.path
-    print(function)
-
-    if function == "/create_hh":
+    if path == "/create_hh":
         return create_hh(request)
-    elif function == "/add_mb":
+    elif path == "/add_mb":
         return add_mb(request)
-    elif function == "/search_hh":
+    elif path == "/search_hh":
         return search_hh(request)
-    elif function == "/list_grants":
+    elif path == "/list_grants":
         return list_grants(request)
 
-    return {"msg": "Unknown request"}, 200
+    return {"msg": "Unknown request"}, 400
