25ade03ab07eb7899dfa4007a948a08bfdbf7e9b
kaiongit <60166885+kaiongit@users.noreply.github.com>
2022-07-11 21:44:43 +0800

    Implement add member function

--- GlobalConstants.py
+++ GlobalConstants.py
@@ -1,2 +1,3 @@
 GCLOUD_PROJECT_NAME: str = "nomadic-thinker-355706"
 FIRESTORE_COLLECTION_NAME: str = "meteor-data"
+FIRESTORE_SUBCOLLECTION_NAME: str = "members"
--- functions/AddMember.py
+++ functions/AddMember.py
@@ -1,4 +1,41 @@
-from flask import request
+import GlobalConstants
+from flask import jsonify, request
+from google.cloud import firestore
+from google.cloud.firestore_v1.collection import CollectionReference
+from google.cloud.firestore_v1.document import DocumentReference
+from google.cloud.firestore_v1.base_document import DocumentSnapshot
+from objects.Person import Person
 
 def add_mb(request: request):
-    pass
+    
+    # Validate person information
+    request_dict: dict = request.get_json(silent=True)
+    request_dict = request_dict if request_dict else {}
+
+    try:
+        person: Person = Person.from_dict(request_dict)
+    except ValueError as e:
+        return jsonify({"msg": str(e)}), 400
+
+    # Get DB
+    client: firestore.Client = firestore.Client(project=GlobalConstants.GCLOUD_PROJECT_NAME)
+    col_ref: CollectionReference = client.collection(GlobalConstants.FIRESTORE_COLLECTION_NAME)
+    
+    # Validate hh_id exists
+    hh_id: str = request_dict.get("hh_id", "")
+    hh_doc_ref: DocumentReference = col_ref.document(hh_id)
+    hh_doc: DocumentSnapshot = hh_doc_ref.get()
+
+    if not hh_doc.exists:
+        return jsonify({"msg": "hh_id not found"}), 404
+    
+    # Create new member in household
+    try:
+        hh_doc_col_ref: CollectionReference = \
+            hh_doc_ref.collection(GlobalConstants.FIRESTORE_SUBCOLLECTION_NAME)
+        _, member_doc_ref = hh_doc_col_ref.add(person.to_dict())
+    except Exception as e:
+        print("ERROR:", str(e))
+        return jsonify({"msg": "An error has occured"}), 500
+
+    return "", 201
