2e413873e52c2bd195aada34e9436684df13068b
kaiongit <60166885+kaiongit@users.noreply.github.com>
2022-07-12 00:43:00 +0800

    Implement ELB grant and YGG grant

--- functions/grants/ELB.py
+++ functions/grants/ELB.py
@@ -1,2 +1,27 @@
-def ELB(households: list):
-    return households
+from datetime import datetime, timedelta
+
+def ELB(households: list) -> list:
+    # Filter households eligible
+    hh_eligible: list = [e for e in households if household_has_members_older_than_55(e)]
+
+    return hh_eligible
+
+def household_has_members_older_than_55(household: dict) -> bool:
+    # Retrieve list of memebers
+    members: list = household["members"]
+
+    # Filter to only include members that are 55 or older
+    above_55s: list = [member for member in members if member_is_older_than_55(member)]
+    
+    # Return True if there is more than 1 member 55 or older
+    return len(above_55s) > 0
+
+def member_is_older_than_55(member: dict) -> bool:
+    # Convert dob into datetime object
+    dob_datetime = datetime.strptime(str(member["dob"]), "%Y%m%d")
+
+    # Calculate age as today's date subtract dob 
+    age = (datetime.today() - dob_datetime) // timedelta(days=365.2425)
+
+    # Return true if age is 55 or above
+    return age >= 55
--- functions/grants/YGG.py
+++ functions/grants/YGG.py
@@ -1,2 +1,15 @@
-def YGG(households: list):
-    return households
+def YGG(households: list) -> list:
+    # Filter households eligible
+    hh_eligible: list = [e for e in households if household_income_less_than_100000(e)]
+
+    return hh_eligible
+
+def household_income_less_than_100000(household: dict) -> bool:
+    # Retrieve list of members
+    members: list = household["members"]
+
+    # Sum the annual_income of all members
+    total_income = sum([member["annual_income"] for member in members])
+
+    # Return True if sum of annual_income is less than 100000
+    return total_income < 100000
