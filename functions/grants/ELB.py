1ad18cf69edbad285aabf54276900868a52fe894
kaiongit <60166885+kaiongit@users.noreply.github.com>
2022-07-12 10:26:21 +0800

    Fix ELB grant not properly filtering eligible members

--- functions/grants/ELB.py
+++ functions/grants/ELB.py
@@ -4,6 +4,12 @@ def ELB(households: list) -> list:
     # Filter households eligible
     hh_eligible: list = [e for e in households if household_has_members_older_than_55(e)]
 
+    # Filter members eligible
+    mb_eligible: list = [only_members_above_16(e) for e in hh_eligible]
+
+    # Remove households with empty members
+    mb_eligible = [e for e in mb_eligible if e]
+    
     return hh_eligible
 
 def household_has_members_older_than_55(household: dict) -> bool:
@@ -25,3 +31,17 @@ def member_is_older_than_55(member: dict) -> bool:
 
     # Return true if age is 55 or above
     return age >= 55
+
+def only_members_above_16(household: dict) -> dict:
+    # Retrieve list of members
+    members: list = household["members"]
+
+    # Filter to only include members that are 55 or older
+    elders: list = [member for member in members if member_is_older_than_55(member)]
+
+    # If there is no eligible members, return nothing
+    if len(elders) == 0: return None
+
+    # Otherwise, return eligible members
+    household["members"] = elders
+    return household
